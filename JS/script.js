ec2cd832bbc78b9e68309bed6452572504e725a0
CJavat <72353993+CJavat@users.noreply.github.com>
2022-07-28 03:27:49 -0500

    Avance del portfolio

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-/* alert("HOLA MUNDO."); */
+let menuMobile = document.querySelector(".menuMobile");
+let barra = document.querySelector(".bar");
+
+menuMobile.addEventListener("dblclick", ()=>{
+    barra.style.transform = "rotate(90deg)";
+    barra.style.transition = "transform 5s";
+});
+
+/* TERMINAR DE HACER LA ANIMACION DE MOVER LAS BARRAS. */
