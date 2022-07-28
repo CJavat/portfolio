d0b7121098ded7635d87741e72537ac23a02b70e
JavatoX <cdpm98@hotmail.com>
2022-07-28 16:24:51 -0500

    Agregar cambios

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,42 @@
-let menuMobile = document.querySelector(".menuMobile");
-let barra = document.querySelector(".bar");
+desplazarMenu();
 
-menuMobile.addEventListener("dblclick", ()=>{
-    barra.style.transform = "rotate(90deg)";
-    barra.style.transition = "transform 5s";
-});
 
-/* TERMINAR DE HACER LA ANIMACION DE MOVER LAS BARRAS. */
+function desplazarMenu() {
+    let listaNavegacion = document.querySelector(".Container__listaNavegacionMobile");
+    let menuMobile = document.querySelector(".menuMobile");
+    let primeraBarra = document.querySelector(".bar:first-child");
+    let ultimaBarra = document.querySelector(".bar:last-child");
+    let bandera = false;
+    
+    menuMobile.addEventListener("click", ()=>{
+        if(bandera === false) {
+            bandera = true;
+            listaNavegacion.style.transition = "transform 1S ease-in-out, visibility 1s ease-in-out"
+            listaNavegacion.className = "Container__listaNavegacionMobileActived";
+            
+            menuMobile.style.transform = "rotate(-45deg)";
+            menuMobile.style.transition = "transform 2s";
+            
+            primeraBarra.style.transform =  "rotate(-90deg) translateY(-7px)";
+            primeraBarra.style.transition = "transform 2s";
+            
+            ultimaBarra.style.transform = "rotate(-90deg) translateY(7px)";
+            ultimaBarra.style.transition = "transform 2s";
+        }
+        else {
+            bandera = false;
+            
+            listaNavegacion.className = "Container__listaNavegacionMobile";
+            listaNavegacion.style.transition = "transform 1S ease-in-out, visibility 1s ease-in-out"
+            
+            menuMobile.style.transform = "rotate(0)";
+            menuMobile.style.transition = "transform 2s";
+        
+            primeraBarra.style.transform =  "rotate(0) translateY(0)";
+            primeraBarra.style.transition = "transform 2s";
+        
+            ultimaBarra.style.transform = "rotate(0) translateY(-0)";
+            ultimaBarra.style.transition = "transform 2s";
+        }
+    });
+}
